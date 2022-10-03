86955495aec91265e925f60f7bf219ab24f6554f
trupti0406 <truptivijayyadav27@gmail.com>
2022-10-03 23:02:44 +0530

    product component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="app">
       {/* <h1>हाँ तेरे चुम्मे में चवनप्राश है </h1> */}
       <Header/>
+      <Home/>
     </div>
   );
 }
