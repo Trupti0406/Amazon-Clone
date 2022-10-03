596f681b44f1fa1eaa87f91f644285506d1fd33f
trupti0406 <truptivijayyadav27@gmail.com>
2022-10-03 19:37:37 +0530

    header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5JVjV-DLXnV6rG5WhsJcOqekhMJ9Gd2A",
+  authDomain: "clone-a8d92.firebaseapp.com",
+  projectId: "clone-a8d92",
+  storageBucket: "clone-a8d92.appspot.com",
+  messagingSenderId: "433627145829",
+  appId: "1:433627145829:web:e4b99dfc48a28ce52a488c",
+};
